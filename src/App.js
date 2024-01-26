59011b98674367374c59ce37b9e8593f6192be02
sandralillepool <145442178+essandora@users.noreply.github.com>
2024-01-26 14:42:31 +0200

    added items and data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    {
+    date: new Date(2023, 9, 6),
+    title: 'New book',
+    price: 30.99
+    },
+    {
+    date: new Date(2023, 9, 6),
+    title: 'New jeans',
+    price: 99.99
+  
+    }
+]
   return (
     <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
-    </div>
+      <ExpenseItem 
+        expenseData={expenses[0]}
+      />
+        <ExpenseItem
+        expenseData={expenses[1]}
+        />
+      </div>
   );
 }
 
